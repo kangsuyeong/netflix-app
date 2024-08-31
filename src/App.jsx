9223543f64c398287e9c92b2e170dabe28a249da
kangsuyeong <ekdh16@naver.com>
2024-08-31 19:14:54 +0900

    오류

--- src/App.jsx
+++ src/App.jsx
@@ -5,7 +5,7 @@ import AppLayout from "./layout/AppLayout";
 import MoviePage from "./pages/Movies/MoviePage";
 import MovieDetailPage from "./pages/MovieDetail/MovieDetailPage";
 import NotFoundPage from "./pages/NotFoundPage/NotFoundPage";
-import HomePage from "./pages/Homepage/HomePage";
+import HomePage from "./pages/Home/HomePage";
 
 // 홈페이지 /
 // 영화 전체보여주는 페이지 (서치) /movies
